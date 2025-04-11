0c7c0aab4e8f61e6ee31ded15a02e6bdfde89475
Ted12m <cooldudeyahya12@gmail.com>
2025-04-11 17:50:28 +0530

    autoplay cancelled

--- cake.js
+++ cake.js
@@ -1,13 +1,14 @@
 const blowing = new Audio('assets/blowing.mp3');
 const HBD = new Audio('assets/HBD.mp3');
+
 $(document).ready(function() {
     $(".candles").click(function() {
-      blowing.play();  
+      blowing.play(); 
+      HBD.play(); 
       $(".flame").animate({"opacity": 0}, "fast");
       $(".flame2").animate({"opacity": 0}, "fast");
       $(".flame3").animate({"opacity": 0}, "fast");
       $(".text").animate({ "top": -300, "opacity": 1 }, "fast");
-        HBD.play();
     confetti({
       particleCount: 120,
       spread: 80,
