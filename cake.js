7d3e09f0bb822b4533bf7bb4157a54d48e52c0c6
Ted12m <cooldudeyahya12@gmail.com>
2025-04-11 17:56:31 +0530

    title changes

--- cake.js
+++ cake.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
       $(".flame").animate({"opacity": 0}, "fast");
       $(".flame2").animate({"opacity": 0}, "fast");
       $(".flame3").animate({"opacity": 0}, "fast");
-      $(".text").animate({ "top": -300, "opacity": 1 }, "fast");
+      $(".text").animate({ "top": -150, "opacity": 1 }, "fast");
     confetti({
       particleCount: 120,
       spread: 80,
